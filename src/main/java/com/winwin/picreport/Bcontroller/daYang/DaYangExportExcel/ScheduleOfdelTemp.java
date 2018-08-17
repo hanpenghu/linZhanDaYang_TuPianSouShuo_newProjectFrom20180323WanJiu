b60925d8a4ec0c81e67c879221b28041f6eb4bae
hanpenghu <563909408@qq.com>
2018-08-17 09:20:32 +0800

    2018_8_17   weekday(5)   9:20:25

--- src/main/java/com/winwin/picreport/Bcontroller/daYang/DaYangExportExcel/ScheduleOfdelTemp.java
+++ src/main/java/com/winwin/picreport/Bcontroller/daYang/DaYangExportExcel/ScheduleOfdelTemp.java
@@ -19,17 +19,38 @@ public class ScheduleOfdelTemp {
 
     @Scheduled(cron = "0 0 23 * * ?")
     public void delTempOfExportExcel(){
-        String s=daYangSuoLueTuAndFuJianZongPath.replace(".","").replace("/","");
-        String path=p.springBootJarPath()+s+File.separator+ Cnst.saveExcelTemp+File.separator;
-        File file = new File(path);
-        if(p.notExists(file))return;
-        if(p.notDic(file))return;
-        File[] files = file.listFiles();
-        if(p.empty(files))return;
-        for(File f:files){
-            f.delete();
+        this.delCountFile();
+        this.delTempExcelExport();
+    }
+
+
+    //删除excel导出的临时文件
+    private void delTempExcelExport(){
+        try {
+            String s=daYangSuoLueTuAndFuJianZongPath.replace(".","").replace("/","");
+            String path=p.springBootJarPath()+s+File.separator+ Cnst.saveExcelTemp+File.separator;
+            File file = new File(path);
+            if(p.notExists(file))return;
+            if(p.notDic(file))return;
+            File[] files = file.listFiles();
+            if(p.empty(files))return;
+            for(File f:files){
+                f.delete();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+    //删除文件导出计数器文件
+    private void delCountFile(){
+        try {
+           p.Del( new File("down"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 
